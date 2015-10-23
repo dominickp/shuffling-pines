2659ef6d1c55f7257930e1e8b375192a9c4f1347
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-22 21:47:21 -0400

    Adding users to an array

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,39 @@
 var app = angular.module('shuffling', [])
     .controller('FormController', [function(){
 
+        vm = this;
+
+        vm.guests = [];
+
+        vm.register = function(){
+          console.log('register');
+
+            // Add the guest to the array
+            vm.guests.push({
+                name: vm.guestName,
+                transitionDate: vm.transitionDate,
+                entryDate: new Date(),
+                action: vm.action,
+                pickupLocation: vm.location
+            });
+            console.log(vm.guests);
+
+            // Empty values
+            delete vm.guestName;
+            delete vm.transitionDate;
+            delete vm.entryDate;
+            delete vm.action;
+            delete vm.location;
+
+
+        };
 
     }])
     .controller('TabController', [function(){
 
+        vm = this;
+
         // Init on first tab
-        this.tab = 1;
+        vm.tab = 1;
 
     }]);
