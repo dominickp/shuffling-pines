05a44d7160e1351e0a047c49d4b32147f785fa5e
Dominick Peluso <peluso.dominick@gmail.com>
2015-10-26 19:58:52 -0400

    Add basic registration tests

--- src/tests/FormControllerSpec.js
+++ src/tests/FormControllerSpec.js
@@ -12,7 +12,16 @@ describe('FormController', function(){
     }));
 
 
-    it('should register guests', function(){
-        expect(true).toBe(true);
+    it('should register a new guests', function(){
+
+        formController.guestName = 'John Harvard';
+        formController.transitionDate = new Date();
+        formController.entryDate = new Date();
+        formController.action = 'dropoff';
+        formController.location = 'Anywhere';
+
+        formController.register();
+
+        expect(formController.guests[0].name).toBe('John Harvard');
     });
 });
